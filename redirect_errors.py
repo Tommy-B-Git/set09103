b0d5e5829a0ad7f7a6a0f5b6ad42ce00cbfb1e98
Tom Brown <40210205@live.napier.ac.uk>
2016-09-22 14:28:04 +0000

    Added custom 404 error handling

--- redirect_errors.py
+++ redirect_errors.py
@@ -7,9 +7,21 @@ def private():
     #so redirect to login URL
     return redirect(url_for('login'))
 
+@app.route("/")
+def hello():
+    return "Hello Napier"
+
 @app.route("/login")
 def login():
     return "Now we would get username and password"
+
+@app.route("/force404")
+def force404():
+    abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "couldn't find the page you requested", 404
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
