0bdec6491b35aaf656a8d84cfe232a1a6607d07e
Tom Brown <40210205@live.napier.ac.uk>
2016-11-03 13:46:47 +0000

    Added tuts up to 6.2 sessions

---
+++ 5.0.4_template_inheritance.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/inherits/')
+def inherits():
+  return render_template('base.html')
+
+#pull in 1st block of content to base.html
+@app.route('/inherits/one/')
+def inherits_one():
+  return render_template('inherits1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+  return render_template('inherits2.html')
+
+if __name__ == ("__main__"):
+  app.run(host='0.0.0.0', debug=True)
+
+
